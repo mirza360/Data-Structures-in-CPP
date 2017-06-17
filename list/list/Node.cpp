52536a3e0bc6c408583245242f383fcaae36f4aa
Mirza Belal Ahmed <mirza360@outlook.com>
2017-06-18 03:14:58 +0600

    linked list struct without header
    No header added so far, struct implementation begins

---
+++ list/list/Node.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+//#include "Node.h"
+#include<iostream>
+
+using namespace std;
+struct Node
+{
+
+
+
+public:
+	int value;
+	Node *next=NULL;
+	//Node();
+	//void setValue(int x);
+	//~Node();
+};
+Node*head = NULL;
---
+++ list/list/list.cpp
@@ -0,0 +1,15 @@
+// list.cpp : Defines the entry point for the console application.
+//
+#include "stdafx.h"
+//#include"Node.h"
+#include "Node.cpp"
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello" << endl;
+    return 0;
+}
+
