330ee7c155c73d3b568d9c349080ca5017eabf7f
Mirza Belal Ahmed <mirza360@outlook.com>
2017-06-18 04:10:11 +0600

    Add to the end & print

--- list/list/Node.cpp
+++ list/list/Node.cpp
@@ -5,14 +5,30 @@
 using namespace std;
 struct Node
 {
-
-
-
-public:
 	int value;
+	
 	Node *next=NULL;
-	//Node();
-	//void setValue(int x);
-	//~Node();
+
+	void printNode(Node *head) {
+		Node*idx=head;
+		while (idx != NULL) {
+			cout << idx->value << " ";
+			idx = idx->next;
+		}
+		cout << endl;
+	}
+	void addNodeToEnd(Node *n, Node* head) {
+		if (head->next == NULL) {
+			head->next = n;
+		}
+		else {
+			Node *idx = head;
+			while (idx->next != NULL) {
+				idx = idx->next;
+			}
+			idx->next = n;
+		}
+	}
+
 };
-Node*head = NULL;
+//Node*head = NULL;
--- list/list/list.cpp
+++ list/list/list.cpp
@@ -7,9 +7,40 @@
 
 using namespace std;
 
+
 int main()
 {
-	cout << "Hello" << endl;
+	Node*head = NULL;
+	Node*n = new Node();
+	head = n;
+	cout << "Let's Create the first link!"<<endl<<"Please Enter the value: ";
+	int v;
+	cin >> v;
+	n->value = v;
+	//cout << n->value << endl;
+	int idx = 0;
+	cout << "We'll be adding values to the end" << endl;
+	while (idx!=5) {
+		cout << "To continue adding press 1, to print press 2 and to terminate press 5" << endl;
+		cin >> idx;
+		switch (idx) {
+		case 1: {
+			cout << "Enter Your Value: ";
+			int val;
+			cin >> val;
+			Node*tmp = new Node();
+			tmp->value = val;
+			tmp->addNodeToEnd(tmp, head);
+		}break;
+		case 2: {
+			cout << "Printing The Values!" << endl;
+			n->printNode(head);
+		}break;
+		case 5: {
+			cout << "Terminating!" << endl;
+		}break;
+		}
+	}
     return 0;
 }
 
